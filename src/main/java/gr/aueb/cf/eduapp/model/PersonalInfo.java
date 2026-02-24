8a946092c2e3b48d7be86a20cddebffdc70da69e
Iliaskokkal <kokkiliasgrandera@gmail.com>
2026-02-24 19:57:07 +0200

    migration db

---
+++ src/main/java/gr/aueb/cf/eduapp/model/PersonalInfo.java
@@ -0,0 +1,4 @@
+package gr.aueb.cf.eduapp.model;
+
+public class PersonalInfo {
+}
---
+++ src/test/java/gr/aueb/cf/edu_rest_app_pro/EduRestAppProApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.edu_rest_app_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduRestAppProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
